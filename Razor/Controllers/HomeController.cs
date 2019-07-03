8abc224c6c4327e5059ccaa276bbf5f9d19f29fb
Wojciech Stylok <wojciech@stylok.pl>
2019-07-04 00:39:26 +0200

    base of application created

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductId = 1,
+                Name = "Kajak",
+                Description = "Jednnosobowy kajak",
+                Price = 275M,
+                Category = "Sporty wodne"
+            };
+
+            return View(myProduct);
+        }
+    }
+}
